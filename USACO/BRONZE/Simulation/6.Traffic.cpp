9f3c936a748f2f3807a804bcb4ab8a1a25e5f9a5
Rajat Shenoi <48760956+RajatShenoi@users.noreply.github.com>
2022-06-16 19:50:08 +0530

    Update 6.Traffic.cpp

--- USACO/BRONZE/Simulation/6.Traffic.cpp
+++ USACO/BRONZE/Simulation/6.Traffic.cpp
@@ -1,43 +1,68 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
-using namespace std;
+using std::cout;
+using std::endl;
+using std::vector;
 
-const int inf = 10e9;
+// An arbitrary number that's large compared to the ones in the problem.
+const int LARGE = 1e9;
 
-int main(int argc, char const *argv[])
-{
-	int N;
-	cin >> N;
+int main() {
+	std::ifstream read("traffic.in");
+	int num_miles;
+	read >> num_miles;
 
-	low = inf;
-	high = -inf;
+	vector<std::string> segment_type(num_miles);
+	vector<int> start(num_miles);
+	vector<int> end(num_miles);
+	for (int m = 0; m < num_miles; m++) {
+		read >> segment_type[m] >> start[m] >> end[m];
+	}
 
-	for (int i = 0; i < N; i++)
-	{
-		string type;
-		cin >> type;
-
-		int n, m;
-		cin >> n >> m;
-
-		if (type == "none")
-		{
-			low = max(low, n);
-			high = min(high, m);
-		}
-		if (type == "off")
-		{
-			low += n;
-			high += n;
-		}
-		if (type == "on")
-		{
-			low -= n;
-			high -= m;
-
-			low = max(0, low);
+	// Set a large range that might as well be (-infinity, infinity)
+	int low = -LARGE;
+	int high = LARGE;
+	for (int m = num_miles - 1; m >= 0; m--) {
+		if (segment_type[m] == "none") {
+			// Set a new range based on sensor reading.
+			low = std::max(low, start[m]);
+			high = std::min(high, end[m]);
+		} else if (segment_type[m] == "off") {
+			// Update range of possible traffic flows
+			low += start[m];
+			high += end[m];
+		} else if (segment_type[m] == "on") {
+			low -= end[m];
+			high -= start[m];
+			// Set to zero if low becomes negative
+			low = std::max(0, low);
 		}
 	}
+
+	std::ofstream write("traffic.out");
+	write << low << ' ' << high << endl;
+
+	low = -LARGE;
+	high = LARGE;
 	
-	return 0;
+	// Process again, this time scanning the other way
+	for (int m = 0; m < num_miles; m++) {
+		if (segment_type[m] == "none") {
+			low = std::max(low, start[m]);
+			high = std::min(high, end[m]);
+		} else if (segment_type[m] == "on") {
+			low += start[m];
+			high += end[m];
+		} else if (segment_type[m] == "off") {
+			low -= end[m];
+			high -= start[m];
+			low = std::max(0, low);
+		}
+	}
+
+	write << low << ' ' << high << endl;
 }
